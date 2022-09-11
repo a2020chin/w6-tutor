fbce2afb56c0c4b250527fda62fe111e6f898b05
a2020chin <sshane258@gmail.com>
2022-09-11 21:46:35 +0900

    Update 2022-09-11T12:46:20.596Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,18 @@
+"use strict";
+
 var swiper = new Swiper(".programSwiper", {
   slidesPerView: 3,
   spaceBetween: 16,
   loop: true,
   autoplay: {
-    delay: 2000,
+    delay: 2000
   },
   breakpoints: {
     /* 螢幕寬度大於等於 992px 時切換為 3 欄 */
     992: {
       slidesPerView: 7
     },
+
     /* 螢幕寬度大於等於 768px 時切換為 2 欄 */
     768: {
       slidesPerView: 3
@@ -20,29 +23,26 @@ var swiper = new Swiper(".programSwiper", {
     0: {
       slidesPerView: 1
     }
-  },
+  }
 });
-
 var swiper = new Swiper(".mySwiper", {
   effect: "fade",
   loop: true,
-  
   fadeEffect: {
     crossFade: true
   },
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-prev"
   },
   pagination: {
-    el: ".swiper-pagination",
-    
-    // clickable: true,
-  },
-});
+    el: ".swiper-pagination" // clickable: true,
 
-$('.sm-menu').on('click',function (event) {
+  }
+});
+$('.sm-menu').on('click', function (event) {
   event.preventDefault();
   $('.sm-nav').stop().slideUp();
   $('.sm-nav').stop().slideToggle();
 });
+//# sourceMappingURL=all.js.map
